5b95e828dd068eab25d4fbdd3239811e7ae53fed
Brian M <74316375+iSpireCode@users.noreply.github.com>
2022-12-27 17:45:18 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+# todos = []
+#
+# while True:
+#
+#     user_action = input("Please enter add, show, or exit: ")
+#     user_action = user_action.strip()
+#
+#     match user_action:
+#         case 'add':
+#             todo = input("Please enter an action. ")
+#             todos.append(todo)
+#         case 'show':
+#             for item in todos:
+#                 item.capitalize()
+#                 print(item)
+#
+#         case 'edit':
+#             number = int(input("Please enter a number: "))
+#             number -= 1 #index
+#             todos[number] = input("Please give a new task. ")
+#             print(todos)
+#         case 'exit':
+#             break
+#         case taskNoKnown:
+#             print("Could you please enter identifiable task? ")
+#
+# print("Signing off! ")
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
